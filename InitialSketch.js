1987fcc3ddc14963b7eeda0786e35e1f6b857689
chronometry <hereisharrys@gmail.com>
2022-08-01 13:03:15 -0700

    Added a stylesheet and centered my divs

--- InitialSketch.js
+++ InitialSketch.js
@@ -1,25 +1,26 @@
 let angle = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  angleMode(DEGREES);
-  rectMode(CENTER);
+    var canvas = createCanvas(400, 400);
+    canvas.parent('sketch-holder')
+    angleMode(DEGREES);
+    rectMode(CENTER);
 }
 
 function draw() {
-  background(0);
+    background(0);
 
-  push();
-  translate(50, 50);
-  rotate(angle);
-  fill(255, 100, 50);
-  rect(0, 0, 100, 50);
-  pop();
+    push();
+    translate(50, 50);
+    rotate(angle);
+    fill(255, 100, 50);
+    rect(0, 0, 100, 50);
+    pop();
 
-  translate(300, 300);
-  rotate(-angle * 3);
-  fill(50, 100, 250);
-  rect(0, 0, 100, 50);
+    translate(300, 300);
+    rotate(-angle * 3);
+    fill(50, 100, 250);
+    rect(0, 0, 100, 50);
 
-  angle = angle + 5;
+    angle = angle + 5;
 }
