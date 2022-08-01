b543a68bebf3b227151d68960d168b5b4196cb09
chronometry <hereisharrys@gmail.com>
2022-08-01 13:48:56 -0700

    Moved the project to typescript with npm

---
+++ src/InitialSketch.ts
@@ -0,0 +1,34 @@
+import P5 from "p5";
+import "p5/lib/addons/p5.dom";
+
+const sketch = (p5: P5) => {
+    let angle = 0;
+
+    p5.setup = () => {
+        const canvas = p5.createCanvas(400, 400);
+        canvas.parent('app')
+
+        p5.angleMode(p5.DEGREES);
+        p5.rectMode(p5.CENTER);
+    };
+
+    p5.draw = () => {
+        p5.background(0);
+
+        p5.push();
+        p5.translate(50, 50);
+        p5.rotate(angle);
+        p5.fill(255, 100, 50);
+        p5.rect(0, 0, 100, 50);
+        p5.pop();
+
+        p5.translate(300, 300);
+        p5.rotate(-angle * 3);
+        p5.fill(50, 100, 250);
+        p5.rect(0, 0, 100, 50);
+
+        angle = angle + 5;
+    };
+};
+
+new P5(sketch);
